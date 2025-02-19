0ee19c40e1c02fa5b14fe7b49e1ec4c728f29bc9
Jack Langille <lwj1396@proton.me>
2025-02-19 19:40:12 -0400

    work

--- src/server.cpp
+++ src/server.cpp
@@ -10,11 +10,13 @@ int main()
     std::atomic<bool> running(true);
 
     UART serverUart(SERVER_PORT);
+
     if (!serverUart.isOpen())
     {
         std::cerr << "[SERVER] Cannot open: " << SERVER_PORT << "\n";
         return -1;
     }
+
     std::cout << "[SERVER] Listening on " << SERVER_PORT << std::endl;
 
     uartReadLoop(serverUart, running);
